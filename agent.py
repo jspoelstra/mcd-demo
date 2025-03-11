9b96907511755f0e736a4fe1f5cbd72ad8958ea5
Jacob Spoelstra <jaspoels@microsoft.com>
2025-03-11 12:56:17 -0700

    Add agent module for asynchronous interaction with math and weather servers

---
+++ agent.py
@@ -0,0 +1,39 @@
+import asyncio
+from dotenv import load_dotenv
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import AzureChatOpenAI
+
+load_dotenv()
+model = AzureChatOpenAI(
+    azure_deployment="gpt-4o-mini",
+    api_version="2025-01-01-preview",
+)
+
+
+async def main():
+    async with MultiServerMCPClient(
+        {
+            "math": {
+                # make sure you start your weather server on port 5001
+                "url": "http://localhost:5001/sse",
+                "transport": "sse",
+            },
+            "weather": {
+                # make sure you start your weather server on port 5002
+                "url": "http://localhost:5002/sse",
+                "transport": "sse",
+            },
+        }
+    ) as client:
+        agent = create_react_agent(model, client.get_tools())
+        math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+        print(math_response)
+        weather_response = await agent.ainvoke(
+            {"messages": "what is the weather in nyc?"}
+        )
+        print(weather_response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
