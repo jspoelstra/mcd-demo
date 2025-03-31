e7d7324faa6d4091d54d54fabbb4fdeb93c87f59
Jacob Spoelstra <jaspoels@microsoft.com>
2025-03-31 15:09:29 -0700

    Refactor main function to retrieve MATH_SERVER_URI from environment variable and update model initialization

--- agent.py
+++ agent.py
@@ -1,19 +1,19 @@
+import os
 import asyncio
 from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
 from langchain_openai import AzureChatOpenAI
 
-load_dotenv()
-model = AzureChatOpenAI(
-    azure_deployment="gpt-4o-mini",
-    api_version="2025-01-01-preview",
-)
+async def main(model):
+    # Get MATH_SERVER_URI from environment variable
+    MATH_SERVER_URI = os.getenv("MATH_SERVER_URI", "http://localhost:5001/sse")
 
-
-async def main():
+    # Define the questions to ask the agent
     questions = [
         "what's (3 + 5) x 12?",
+        "what's 1234 / 23?",
+        "what's 2 ^ 7?",
         "what is the weather in nyc?",
         "What is the average temperature of the pump?",
         "What is the median flow rate of the pump?",
@@ -27,7 +27,7 @@ async def main():
         {
             "math": {
                 # make sure you start your math server on port 5001
-                "url": "http://localhost:5001/sse",
+                "url": MATH_SERVER_URI,
                 "transport": "sse",
             },
             "weather": {
@@ -45,8 +45,18 @@ async def main():
         agent = create_react_agent(model, client.get_tools())
         for question in questions:
             response = await agent.ainvoke({"messages": question})
-            print(response)
+            print(response["messages"][-1].content)
+            # print(response)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # Load environment variables from .env file
+    load_dotenv()
+    # Set up the model
+    model = AzureChatOpenAI(
+        azure_deployment="gpt-4o-mini",
+        api_version="2025-01-01-preview",
+    )
+
+
+    asyncio.run(main(model))
